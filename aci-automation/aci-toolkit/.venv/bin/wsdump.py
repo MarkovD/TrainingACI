f907ef89f55c14757bd5c7eef577731d4d74bc12
MarkovD <m.daurelio@hotmail.it>
2020-11-07 12:57:28 +0100

    virtualenv fix

---
+++ aci-automation/aci-toolkit/.venv/bin/wsdump.py
@@ -0,0 +1,4 @@
+#!/home/marco/Lavoro/TrainingACI/aci-automation/aci-toolkit/.venv/bin/python3
+# EASY-INSTALL-SCRIPT: 'websocket-client==0.57.0','wsdump.py'
+__requires__ = 'websocket-client==0.57.0'
+__import__('pkg_resources').run_script('websocket-client==0.57.0', 'wsdump.py')
